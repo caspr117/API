d6d103c9ee5a19fc286ea8d4ae30eb9bc1abda88
caspr117 <caspr117@gmail.com>
2025-06-22 12:49:13 -0400

    2nd commit

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const dotenv = require('dotenv')
+const express = require("express");
+const dotenv = require("dotenv");
 
-dotenv.config({path: './config/config.env'})
-const app = express()
-const PORT = process.env.PORT || 5000
+const bootcamps = require("./routes/bootcamps");
+dotenv.config({ path: "./config/config.env" });
+const app = express();
+
+app.use("/api/v1/bootcamps", bootcamps);
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
